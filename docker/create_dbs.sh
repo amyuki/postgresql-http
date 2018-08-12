3caffc89c652233bc7aed05de2ffd994588aff5f
Michael Shen <mshen@sparkpad.com>
2018-08-12 10:45:15 +0800

    add init operations

--- docker/create_dbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#set -e
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-#    CREATE USER etl;
-#    CREATE DATABASE etl;
-#    GRANT ALL PRIVILEGES ON DATABASE etl TO etl;
-#EOSQL
--- docker/create_extensions.sql
+++
@@ -1,5 +0,0 @@
-create extension IF NOT EXISTS file_fdw;
-create extension IF NOT EXISTS postgres_fdw;
-create extension IF NOT EXISTS "uuid-ossp";
-create extension IF NOT EXISTS http;
-CREATE EXTENSION IF NOT EXISTS pg_cron;
