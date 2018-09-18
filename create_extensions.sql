28481e60eeb97c89d18133caddd2b4b0b288e6e8
Y.Cheung <amyuki@users.noreply.github.com>
2018-09-18 15:06:06 +0800

    Update create_extensions.sql

--- create_extensions.sql
+++ create_extensions.sql
@@ -2,4 +2,3 @@ create extension IF NOT EXISTS file_fdw;
 create extension IF NOT EXISTS postgres_fdw;
 create extension IF NOT EXISTS "uuid-ossp";
 create extension IF NOT EXISTS http;
-CREATE EXTENSION IF NOT EXISTS pg_cron;
